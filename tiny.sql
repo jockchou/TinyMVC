137ae9c5fbe6371b9dea464f799424e3583aa727
zhoujing223 <zhoujing223@pingan.com.cn>
2016-08-30 13:48:07 +0800

    line sep LF

---
+++ tiny.sql
@@ -0,0 +1,34 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : 127.0.0.1
+Source Server Version : 50710
+Source Host           : 127.0.0.1:3306
+Source Database       : tiny
+
+Target Server Type    : MYSQL
+Target Server Version : 50710
+File Encoding         : 65001
+
+Date: 2016-08-30 13:19:23
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `userid` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `addtime` datetime DEFAULT NULL,
+  PRIMARY KEY (`userid`),
+  UNIQUE KEY `idx_username` (`username`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES ('1', 'JockChou', 'jockchou@qq.com', '2016-08-30 13:06:54');
